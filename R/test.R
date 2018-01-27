399c9edc662caabaa7908032dd408d8b5e48786a
GeneTinderholm <tinderholmgene@gmail.com>
2018-01-27 12:17:08 -0600

    test

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' plusOne
+#'
+#' This function adds one to a number
+#' @param number
+#' @keywords test
+#' @export
+#' @examples
+#' plusOne(5)
+
+plusOne <- function(number){
+    number + 1
+}
---
+++ R/testTwo.R
@@ -0,0 +1,12 @@
+#' plusTwo
+#'
+#' This function adds two to a number
+#' @param number
+#' @keywords test
+#' @export
+#' @examples
+#' plusTwo(5)
+
+addTwo <- function(number) {
+    number + 2
+}
